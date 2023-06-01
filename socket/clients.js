fb8738a8666285413e9e86e7a3bb87082f9a3b83
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2023-06-01 10:15:48 +0200

    all mqtt

--- socket/clients.js
+++ socket/clients.js
@@ -1,4 +1,5 @@
 const clients = require('mqtt')
+const {messaging} = require("firebase-admin");
 const client = clients.connect('mqtt://test.mosquitto.org')
 require('dotenv').config()
 const TOPIC = process.env.TOPIC;
@@ -17,7 +18,7 @@ client.on('message', function (topic, message) {
   let playersList = getVariable()
 
   // Switch
-  switch (message.toString()){
+  switch (topic){
     // New game
     case `${TOPIC}/NewGame`:
       // START A NEW GAME
@@ -25,7 +26,7 @@ client.on('message', function (topic, message) {
 
     // Good Answer
     case `${TOPIC}/GoodAnswer`:
-      const found = playersList.find(player => player.color === message)
+      const found = playersList.find(player => player.remote64 === message.toString())
       if(found !== undefined){
         found.addScore();
         setVariable(playersList)
@@ -41,10 +42,6 @@ client.on('message', function (topic, message) {
   }
 })
 }
-// WINNER A -> C
-// POP-UP QUESTION A -> C
-// JOUEUR A REJOINT A -> C
-
 
 function send_winner(winner){
     client.publish(`${TOPIC}/winner`, winner)
@@ -62,6 +59,7 @@ module.exports.send_winner = send_winner
 module.exports.pop_up = pop_up
 module.exports.player_join = player_join
 
+
 /// CLIENT
 function clientMQTT() {
   client.on('connect', function () {
@@ -73,13 +71,35 @@ function clientMQTT() {
 })
 
 client.on('message', function (topic, message) {
-  // message is Buffer
-  console.log(topic, message.toString())
-  switch (message.toString()){
-    // New game
-    case `${TOPIC}/NewGame`:
-      // START A NEW GAME
+  // SWITCH
+  switch (topic){
+    // Player joined
+    case `${TOPIC}/player`:
+      console.log(`${message.toString()} joined`)
       break;
+
+      // Pop Up
+    case `${TOPIC}/pop_up`:
+      const message_array = message.toString().split('|')
+      const player = message_array[0]
+      const id = message_array[1]
+      console.log('SHOW POP UP SCREEN FOR PLAYER: ' + player + ' WITH ID: ' + id)
+      break;
+
+    case `${TOPIC}/winner`:
+      console.log('WINNER IS PLAYER: ' + message.toString())
   }
 })
+}
+
+function new_game(){
+    client.publish(`${TOPIC}/newGame`, 'new Game')
+}
+
+function goodAnswer(id){
+  client.publish(`${TOPIC}/GoodAnswer`, id)
+}
+
+function pass(){
+    client.publish(`${TOPIC}/pass`, 'PASS')
 }
